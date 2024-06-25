a6bfd867905ab22e9a1106ce2bb87911b79245bb
Srivatsav <srivatsav@Srivatsavs-MacBook-Pro.local>
2024-06-25 23:42:29 +0530

    update logger message format

--- src/middlewares/winston.middleware.ts
+++ src/middlewares/winston.middleware.ts
@@ -10,7 +10,7 @@ const morganMiddleware = morgan(morganFormat, {
         method: message.split(" ")[0],
         url: message.split(" ")[1],
         status: message.split(" ")[2],
-        responseTime: message.split(" ")[3],
+        responseTime: `${message.split(" ")[3]} ms`,
       };
       logger.info(JSON.stringify(logObject));
     },
