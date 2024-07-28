130ab0406984690801d87774324676ce8b9c01ef
Srivatsav K <srivatsav@Srivatsavs-MacBook-Pro.local>
2024-07-28 17:02:59 +0530

    add healthcheck router catch all

--- src/routes/healthcheck.routes.ts
+++ src/routes/healthcheck.routes.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
 import { healthCheckController } from "../controllers/healthcheck.controller";
 
 export const healthCheckRouter = Router();
 
 healthCheckRouter.get("/", healthCheckController);
+
+healthCheckRouter.all("*", (_, res) => {
+  res.status(StatusCodes.NOT_FOUND).json({ message: ReasonPhrases.NOT_FOUND });
+});
