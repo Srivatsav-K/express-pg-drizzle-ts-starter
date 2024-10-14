227bb1734e60dcfa5db717c3b605350efad50f89
Srivatsav K <srivatsav@Srivatsavs-MacBook-Pro.local>
2024-10-14 16:17:36 +0530

    update healthcheck route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,10 @@ import { Router } from "express";
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
 import { healthCheckRouter } from "./healthcheck.routes";
 
-const BASE_URL_V1 = "/api/v1";
 const appRouter = Router();
 
 // Health check
-appRouter.use(`${BASE_URL_V1}/health-check`, healthCheckRouter);
+appRouter.use(`/api/health-check`, healthCheckRouter);
 
 appRouter.all("*", (_, res) => {
   res.status(StatusCodes.NOT_FOUND).json({ message: ReasonPhrases.NOT_FOUND });
